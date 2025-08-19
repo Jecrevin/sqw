818081ff509e40d1b6151e11e8a0eadda22b33b1
Haoran Jiang <jianghr@ihep.ac.cn>
2025-08-19 05:09:41 +0000

    refactor: remove redundant file existence check and improve error messages in get_data_from_h5py function

--- src/h2o_sqw_calc/io.py
+++ src/h2o_sqw_calc/io.py
@@ -1,5 +1,3 @@
-import os
-
 import h5py
 import numpy as np
 from numpy.typing import NDArray
@@ -10,16 +8,12 @@ class NotH5FileError(Exception):
 
 
 def get_data_from_h5py[T: np.number](file_path: str, dataset_name: str) -> NDArray[T]:
-    if not os.path.exists(file_path):
-        raise FileNotFoundError(f'The file "{file_path}" does not exist.')
-
     try:
         with h5py.File(file_path, "r") as f:
-            if dataset_name not in f:
-                raise KeyError(f'The dataset "{dataset_name}" does not exist in the file "{file_path}".')
             if not isinstance((dataset := f[dataset_name]), h5py.Dataset):
                 raise TypeError(f'"{dataset_name}" is not a valid HDF5 dataset in the file "{file_path}".')
-
             return dataset[()]
     except OSError as e:
-        raise NotH5FileError(f'The file "{file_path}" is not a valid HDF5 file.') from e
+        raise NotH5FileError(f'Could not open "{file_path}" as an HDF5 file.') from e
+    except KeyError as e:
+        raise KeyError(f'"{dataset_name}" not found in HDF5 file "{file_path}".') from e
