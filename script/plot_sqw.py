53647a4cf74f5f3f54a1d0d1daa94b9a404bc9ae
Haoran Jiang <jianghr@ihep.ac.cn>
2025-09-10 07:52:19 +0000

    feat(plot): make plot comparison to STC optional

--- script/plot_sqw.py
+++ script/plot_sqw.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+from functools import partial
 from typing import Final, Literal
 
 import numpy as np
@@ -22,8 +23,8 @@ def main():
     args = parser.parse_args()
 
     GAMMA_FILE_PATH: Final[str] = args.gamma_file_path
-    STC_FILE_PATH: Final[str] = args.stc_file_path
     ELEMENT: Final[Literal["H", "O"]] = args.element
+    STC_FILE_PATH: Final[str | None] = args.stc_file_path
     TEMPERATURE: Final[float] = args.temperature  # unit: K
     SCALE: Final[Literal["linear", "log"]] = args.scale
     ENERGY_UNIT: Final[bool] = args.energy_unit
@@ -41,42 +42,60 @@ def main():
         sys.exit(f"Error occured while reading gamma data: {e}")
 
     print("Gamma data loaded successfully.")
-    print(f"Reading STC model data from {STC_FILE_PATH}...")
-
-    try:
-        freq_dos, dos = get_stc_model_data(STC_FILE_PATH, element=ELEMENT)
-    except Exception as e:
-        sys.exit(f"Error occured while reading STC data: {e}")
-
-    print("STC model data loaded successfully.")
     print("Calculating S(q,w) using CDFT...")
 
-    omega_vals: tuple[Array1D[np.float64], ...]
-    sqw_vals: tuple[Array1D[np.complex128], ...]
-    omega_vals, sqw_vals = zip(*map(lambda q: sqw_cdft(q, time_vec, gamma_qtm), Q_VALUES), strict=True)
-
-    print("CDFT calculation complete.")
-    print("Calculating S(q,w) using STC model...")
-
-    stc_sqw_vals: list[Array1D[np.float64]] = list(
-        map(lambda q, omega: sqw_stc_model(q, omega, freq_dos, dos, TEMPERATURE), Q_VALUES, omega_vals)
+    omega_vals, sqw_cdft_results = zip(
+        *map(partial(sqw_cdft, time_vec=time_vec, gamma=gamma_qtm), Q_VALUES), strict=True
     )
 
-    print("STC model calculation complete.")
-    print("Plotting S(q,w) comparison...")
+    print("CDFT calculation complete.")
 
-    plt.figure(figsize=(8, 6), layout="constrained")
-    for q, omega, sqw, stc_sqw in zip(Q_VALUES, omega_vals, sqw_vals, stc_sqw_vals, strict=True):
-        x = omega * HBAR if ENERGY_UNIT else omega
-        (stc_line,) = plt.plot(x, np.abs(sqw), label="CDFT Model")
-        plt.plot(x, stc_sqw, label=f"STC Model    for {q = :.2f}", linestyle="--", color=stc_line.get_color())
-    plt.xlim(-10 if ENERGY_UNIT else -10 / HBAR)
-    if SCALE == "log":
-        plt.yscale("log")
-    plt.xlabel("Energy (eV)" if ENERGY_UNIT else "Angular Frequency (rad/s)", fontsize=14)
-    plt.ylabel(r"Scattering Function $S(q,\omega)$ (b·eV⁻¹·Sr⁻¹·ℏ⁻¹)", fontsize=14)
+    if STC_FILE_PATH:
+        print(f"Reading STC model data from {STC_FILE_PATH}...")
+
+        try:
+            freq_dos, dos = get_stc_model_data(STC_FILE_PATH, element=ELEMENT)
+        except Exception as e:
+            sys.exit(f"Error occured while reading STC data: {e}")
+
+        print("STC model data loaded successfully.")
+
+        print("Calculating S(q,w) using STC model...")
+
+        sqw_stc_results = map(
+            partial(sqw_stc_model, freq_dos=freq_dos, density_of_states=dos, temperature=TEMPERATURE),
+            Q_VALUES,
+            omega_vals,
+        )
+        print("STC model calculation complete.")
+    else:
+        sqw_stc_results = None
+
+    print("Plotting S(q,w) results...")
+
+    plt.figure(figsize=(10, 6), layout="constrained")
+    if sqw_stc_results is not None:
+        for q, omega, sqw_cdft_data, sqw_stc_data in zip(
+            Q_VALUES, omega_vals, sqw_cdft_results, sqw_stc_results, strict=True
+        ):
+            x = omega * HBAR if ENERGY_UNIT else omega
+            (line,) = plt.plot(x, np.abs(sqw_cdft_data), label="CDFT model")
+            plt.plot(x, sqw_stc_data, label=f"STC model    for {q = :.2f}", color=line.get_color(), linestyle="--")
+            plt.legend(*reorder_legend_by_row(*plt.gca().get_legend_handles_labels(), ncol=2), ncol=2, loc="upper left")
+    else:
+        for q, omega, sqw_cdft_data in zip(Q_VALUES, omega_vals, sqw_cdft_results, strict=True):
+            x = omega * HBAR if ENERGY_UNIT else omega
+            plt.plot(x, np.abs(sqw_cdft_data), label=f"{q = :.2f}")
+            plt.legend(loc="upper left")
+    plt.yscale(SCALE)
+    x_min = -10 if ENERGY_UNIT else -10 / HBAR
+    x_max = 2 if ENERGY_UNIT else 2 / HBAR
+    plt.xlim(max(plt.xlim()[0], x_min), min(plt.xlim()[1], x_max))
     plt.grid()
-    plt.legend(*reorder_legend_by_row(*plt.gca().get_legend_handles_labels(), ncol=2), ncol=2, loc="upper left")
+    plt.xlabel("Energy (eV)" if ENERGY_UNIT else r"Angular Frequency $\omega$ (rad/s)", fontsize=14)
+    plt.ylabel(r"Scattering Function $S(q,\omega)$ (b·eV⁻¹·Sr⁻¹·ℏ⁻¹)", fontsize=14)
+
+    print("Plotting completed.")
 
     save_or_show_plot(OUTPUT)
 
@@ -88,11 +107,6 @@ def setup_parser() -> argparse.ArgumentParser:
         description=("Plot S(q,w) comparison between CDFT and STC model for given Momentum Transfer values.")
     )
     parser.add_argument("gamma_file_path", type=str, help="File path to the HDF5 file containing the gamma data.")
-    parser.add_argument(
-        "stc_file_path",
-        type=str,
-        help="File path to the HDF5 file containing the STC model data.",
-    )
     parser.add_argument(
         "q_values",
         nargs="+",
@@ -106,6 +120,12 @@ def setup_parser() -> argparse.ArgumentParser:
         choices=["H", "O"],
         default="H",
         help="Element symbol for which to plot the S(q,w) function (default: H).",
+    )
+    parser.add_argument(
+        "-stc",
+        "--stc-file-path",
+        type=str,
+        help="File path to the HDF5 file containing the STC model data.",
     )
     parser.add_argument(
         "-t",
@@ -130,9 +150,9 @@ def setup_parser() -> argparse.ArgumentParser:
         "-o",
         "--output",
         nargs="?",
-        const="fig/sqw_comparison_plot.png",
+        const="fig/sqw_plot.png",
         type=str,
-        help="Output file name for the plot (default: fig/sqw_comparison_plot.png).",
+        help="Output file name for the plot (default: fig/sqw_plot.png).",
     )
     return parser
 
